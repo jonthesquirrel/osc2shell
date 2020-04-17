e7234f99ca7fc8b2040db3957fcfab10a3345f68
Jonathan Herman <jdh11235@gmail.com>
2020-04-17 04:44:24 -0700

    Standardize code formatting

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var fs = require("fs")
-var osc = require("osc")
+let fs = require("fs")
+let osc = require("osc")
 
 let config
 try {
@@ -9,73 +9,71 @@ try {
   console.log(e)
 }
 
-// Create an osc.js UDP Port listening on port 57121.
-var udpPort = new osc.UDPPort({
-    localAddress: "127.0.0.1",
-    localPort: 8000,
-    metadata: true
-});
+// Create a UDP Port to listen for OSC messages
+let udpPort = new osc.UDPPort({
+  localAddress: "127.0.0.1",
+  localPort: 8000,
+  metadata: true
+})
 
 // Run shell commands
 function runShell (command) {
-  const { exec } = require("child_process");
+  const { exec } = require("child_process")
   exec(command, (error, stdout, stderr) => {
     if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+      console.log(`error: ${error.message}`)
+      return
     }
     if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+      console.log(`stderr: ${stderr}`)
+      return
     }
-    // console.log(`stdout: ${stdout}`);
-});
+    // console.log(`stdout: ${stdout}`)
+  })
 }
 
 let last_threshold = {}
 
 // Listen for incoming OSC messages and execute commands based on config
 udpPort.on("message", function (message, timeTag, info) {
-    let address = message.address
-    if (address in config.osc_messages) {
-      let config_item = config.osc_messages[address]
-      let value = message.args[0].value
-      let trigger_type = config_item.trigger_type
-      let threshold = config_item.threshold
-      if (trigger_type == "continuous") {
-        let command = config_item.command
-        command = command.replace("$1", value)
+  let address = message.address
+  if (address in config.osc_messages) {
+    let config_item = config.osc_messages[address]
+    let value = message.args[0].value
+    let trigger_type = config_item.trigger_type
+    let threshold = config_item.threshold
+    if (trigger_type == "continuous") {
+      let command = config_item.command
+      command = command.replace("$1", value)
+      command = `${config.prefix}${command}${config.suffix}`
+      runShell(command)
+    } else if (trigger_type == "threshold") {
+      // Only run once each time threshold is exceeded
+      let command_rising = config_item.command_rising
+      let command_falling = config_item.command_falling
+      if (!(address in last_threshold)) {
+        last_threshold[address] = "below"
+      }
+      if (value >= threshold && last_threshold[address] == "below") {
+        // Rising edge
+        last_threshold[address] = "above"
+        let command = command_rising.replace("$1", value)
         command = `${config.prefix}${command}${config.suffix}`
         runShell(command)
-      } else if (trigger_type == "threshold") {
-        // Only run once each time threshold is exceeded
-        let command_rising = config_item.command_rising
-        let command_falling = config_item.command_falling
-        console.log((value >= threshold), value)
-        // console.log(JSON.stringify(last_threshold))
-        if (!(address in last_threshold)) {
-          last_threshold[address] = "below"
-        }
-        if (value >= threshold && last_threshold[address] == "below") {
-          // Rising edge
-          last_threshold[address] = "above"
-          let command = command_rising.replace("$1", value)
-          command = `${config.prefix}${command}${config.suffix}`
-          runShell(command)
-        } else if (value < threshold && last_threshold[address] == "above") {
-          // Falling edge
-          last_threshold[address] = "below"
-          let command = command_falling.replace("$1", value)
-          command = `${config.prefix}${command}${config.suffix}`
-          runShell(command)
-        }
+      } else if (value < threshold && last_threshold[address] == "above") {
+        // Falling edge
+        last_threshold[address] = "below"
+        let command = command_falling.replace("$1", value)
+        command = `${config.prefix}${command}${config.suffix}`
+        runShell(command)
       }
     }
-});
+  }
+})
 
 udpPort.on("error", function (error) {
-    console.log("An error occurred: ", error.message);
-});
+  console.log("An error occurred: ", error.message)
+})
 
-// Open the socket.
-udpPort.open();
+// Open the socket
+udpPort.open()
