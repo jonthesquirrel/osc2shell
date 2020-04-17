eed28ba91c44dd66b33bf14a6cc6e2bfa3003410
Jonathan Herman <jdh11235@gmail.com>
2020-04-17 02:26:04 -0700

    create config file

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+var fs = require("fs")
 var osc = require("osc")
+
+let config
+const default_config = {
+  "default": "config"
+}
+function initConfig() {
+  var writeStream = fs.createWriteStream("config.json");
+  writeStream.write(JSON.stringify(default_config));
+  writeStream.end();
+}
+try {
+  config = require("./config.json")
+} catch (e) {
+  console.log("No valid config found. Creating one now.")
+  initConfig()
+}
 
 // Create an osc.js UDP Port listening on port 57121.
 var udpPort = new osc.UDPPort({
@@ -9,8 +26,8 @@ var udpPort = new osc.UDPPort({
 
 // Listen for incoming OSC messages.
 udpPort.on("message", function (oscMsg, timeTag, info) {
-    console.log("An OSC message just arrived!", oscMsg);
-    console.log("Remote info is: ", info);
+    // console.log("An OSC message just arrived!", oscMsg);
+    // console.log("Remote info is: ", info);
 });
 
 udpPort.on("error", function (error) {
