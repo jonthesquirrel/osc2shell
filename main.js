5e12008675266636d7c00eb6ac90b0ec582e992e
Jonathan Herman <jdh11235@gmail.com>
2020-04-17 02:13:59 -0700

    works

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var osc = require("osc")
 
 // Create an osc.js UDP Port listening on port 57121.
 var udpPort = new osc.UDPPort({
-    localAddress: "0.0.0.0",
-    localPort: 57121,
+    localAddress: "127.0.0.1",
+    localPort: 8000,
     metadata: true
 });
 
@@ -11,6 +11,10 @@ var udpPort = new osc.UDPPort({
 udpPort.on("message", function (oscMsg, timeTag, info) {
     console.log("An OSC message just arrived!", oscMsg);
     console.log("Remote info is: ", info);
+});
+
+udpPort.on("error", function (error) {
+    console.log("An error occurred: ", error.message);
 });
 
 // Open the socket.
